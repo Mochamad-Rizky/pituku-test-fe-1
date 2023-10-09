32013e8e243d9676b65d43343df4bfb5e4945037
Mochamad Rizky <moch.rizky.cahya@gmail.com>
2023-10-09 12:04:18 +0700

    change: benefit title with margin

--- src/components/Benefits.tsx
+++ src/components/Benefits.tsx
@@ -4,7 +4,7 @@ export default function Benefits() {
   return (
     <section className="bg-benefits-bg bg-cover object-contain py-24">
       <MaxWidthWrapper className="max-w-screen-2xl ">
-        <h2 className="w-full px-3 text-center text-lg font-extrabold leading-normal text-white lg:ml-auto lg:max-w-md lg:text-left lg:text-[3rem]">
+        <h2 className="w-full px-3 text-center text-lg font-extrabold leading-normal text-white lg:ml-auto lg:mr-[15%] lg:max-w-md lg:text-left lg:text-[3rem]">
           Keuntungan Menggunakan Platform Pituku
         </h2>
       </MaxWidthWrapper>
