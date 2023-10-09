5bdbd231c98cac0812287bc2035da017de6345c7
Mochamad Rizky <moch.rizky.cahya@gmail.com>
2023-10-09 09:00:31 +0700

    feat: landing page benefits section

---
+++ src/components/Benefits.tsx
@@ -0,0 +1,13 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+
+export default function Benefits() {
+  return (
+    <section className="bg-benefits-bg bg-cover object-contain py-24">
+      <MaxWidthWrapper className="max-w-screen-2xl ">
+        <h2 className="w-full px-3 text-center text-lg font-extrabold leading-normal text-white lg:ml-auto lg:max-w-md lg:text-left lg:text-[3rem]">
+          Keuntungan Menggunakan Platform Pituku
+        </h2>
+      </MaxWidthWrapper>
+    </section>
+  );
+}
